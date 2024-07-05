f0d936c029b0983787393a2ac6ca76af1ddf21f8
priyatoppo <priyatoppo2304@gmail.com>
2024-07-05 12:38:22 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() 
+{
+    printf("hello priya\n ");
+
+    int x;
+    x = 9;
+    printf("%d",x);
+    // printf("%d,x");
+    x = 7;
+    printf("\n");
+    printf("%d",x);
+
+
+    return 0;
+}
+  
+
