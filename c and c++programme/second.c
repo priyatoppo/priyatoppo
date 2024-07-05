746771d8402f9ea0fc7844c9c814c9098c348e28
priyatoppo <priyatoppo2304@gmail.com>
2024-07-05 11:52:45 +0530

    Add files via upload

---
+++ c and c++programme/second.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("maths is my favourite subject");
+
+ return 0;   
+ #include <stdio.h>
+
+int main()
+{
+    printf("nn\n\nnn\n");
+    printf("nn/n/nnn/n");
+
+    return 0;{
+}
+ #include <stdio.h>
+
+ int main()
+ {
+     printf("hello priya");
+     return 0;
+ }
+
